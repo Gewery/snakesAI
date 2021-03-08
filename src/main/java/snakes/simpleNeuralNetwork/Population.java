6406a38ec2f855c82894b59351927d130aef25fe
Gewery <gewery@mail.ru>
2021-03-08 11:27:46 +0300

    Settings tuned

--- src/main/java/snakes/simpleNeuralNetwork/Population.java
+++ src/main/java/snakes/simpleNeuralNetwork/Population.java
@@ -15,16 +15,16 @@ import static snakes.SnakesUIMain.MAZE_SIZE;
 
 public class Population {
 
-    public static final int POPULATION_SIZE = 40; // (POPULATION_SIZE - ELITISM_COUNT) % 2 == 0 must hold!
-    public static final int ELITISM_COUNT = 10; // must be less or equal to POPULATION_SIZE
-    public static final int PARENTS_SELECTION_GROUP_SIZE = 20;
+    public static final int POPULATION_SIZE = 80; // (POPULATION_SIZE - ELITISM_COUNT) % 2 == 0 must hold!
+    public static final int ELITISM_COUNT = 20; // must be less or equal to POPULATION_SIZE
+    public static final int PARENTS_SELECTION_GROUP_SIZE = 40;
 
     public static final int MAX_LAYERS_NUMBER = 10; // Limit for the number of layers in initial population
     public static final int NEURONS_IN_LAYER = 200; // Number of neurons in one hidden layer
     public static final int MAX_MUTATION_LAYERS_NUMBER = 5; // max number of layers that can be added to a network after mutation
     public static final int STEPS_PER_GAME = 120; // number of steps allowed for one game = 2 mins = 2 * 60
     public static final int NUMBER_OF_TOURNAMENT_RUNS = 1; //each population runs a tournament this number of times
-    public static final double MUTATION_PROBABILITY = 0.2;
+    public static final double MUTATION_PROBABILITY = 0.02;
     public static double CROSSOVER_PROBABILITY = 1;
 
     public final static int FIRST_LAYER_NEURONS_NUMBER = (MAZE_SIZE.x * MAZE_SIZE.y - 2) * 4 + 2;
